a39cc8d5d5fb78558bedf1725d1d8cfaf58dbb5e
henrycaron <57197754+henrycaron@users.noreply.github.com>
2020-10-17 15:44:05 -0400

    Implemented Cross Validation

--- baseline_prob2.py
+++ baseline_prob2.py
@@ -9,6 +9,8 @@ import sklearn.pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils._testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
+from performance_metrics import calc_mean_squared_error
+
 
 
 @ignore_warnings(category=ConvergenceWarning)
@@ -62,7 +64,64 @@ def test_C_vals(x_train_NF, y_train_N, x_valid_MF, y_valid_M):
 
     print("Best C value is:", C_grid[np.argmin(valid_ERs)])
 
+def retrieve_data ():
+    dataset_path = 'data_digits_8_vs_9_noisy'
+    x_train_df = pd.read_csv(os.path.join(dataset_path, 'x_train.csv'))
+    x_train_NF = x_train_df.values
+    N, F = x_train_NF.shape
 
+    y_train_d = pd.read_csv(os.path.join(dataset_path, 'y_train.csv'))
+    y_train_N = y_train_d.values.reshape((N,))
+    
+    return x_train_NF, y_train_N
+
+def train_models_and_calc_scores_for_n_fold_cv(
+        estimator, x_NF, y_N, n_folds=3, random_state=0):
+    
+    train_error_per_fold = np.zeros(n_folds, dtype=np.float)
+    test_error_per_fold = np.zeros(n_folds, dtype=np.float)
+    N, F = x_NF.shape
+  
+    tr_ids_per_f, te_ids_per_f = make_train_and_test_row_ids_for_n_fold_cv(
+                                                        N,n_folds,random_state)
+
+    for i, j, k in zip(tr_ids_per_f, te_ids_per_f, range(n_folds)):
+        estimator.fit(np.take(x_NF, i, axis = 0), np.take(y_N, i))
+        y_hat_tr = estimator.predict(np.take(x_NF, i, axis = 0))
+        y_hat_te = estimator.predict(np.take(x_NF, j, axis = 0))
+        
+        np.put(train_error_per_fold, k, calc_mean_squared_error(np.take(y_N, i), y_hat_tr))
+        np.put(test_error_per_fold, k, calc_mean_squared_error(np.take(y_N, j), y_hat_te))
+         
+    return train_error_per_fold, test_error_per_fold
+
+def make_train_and_test_row_ids_for_n_fold_cv(
+        n_examples=0, n_folds=3, random_state=0):
+        
+    if hasattr(random_state, 'rand'):
+        random_state = random_state 
+    else:
+        random_state = np.random.RandomState(int(random_state))
+    
+    test_array = random_state.permutation(np.arange(n_examples))
+    test_array = np.array_split(test_array, n_folds)
+
+    train_array = list()
+    for i in range(n_folds):
+        train_array.append(np.setxor1d(np.arange(n_examples), test_array[i]))
+    train_ids_per_fold = train_array
+    test_ids_per_fold = test_array
+
+    return train_ids_per_fold, test_ids_per_fold
 
 if __name__ == '__main__':
-    train_i_range()
+    x_train_NF, y_train_N = retrieve_data()
+
+    estimator = sklearn.linear_model.LogisticRegression(C=1.0, solver='lbfgs', max_iter=1000)
+
+    train_err_K, valid_err_K = train_models_and_calc_scores_for_n_fold_cv(estimator, x_train_NF, y_train_N, 3, 1)
+    err_train = np.mean(train_err_K)
+    err_valid = np.mean(valid_err_K)
+    
+    print(err_train, err_valid)
+    # test_C_vals(x_train_NF, y_train_N, x_valid_MF, y_valid_M)
